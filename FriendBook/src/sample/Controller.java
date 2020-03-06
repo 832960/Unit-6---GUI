0753fa7c8d7b957a2231b1dcdf478d2b350311a4
832960 <55299454+832960@users.noreply.github.com>
2020-03-05 18:08:05 -0800

    Friend Book

---
+++ FriendBook/src/sample/Controller.java
@@ -0,0 +1,45 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+
+    public TextField textName;
+    public TextField textPhone;
+    public TextField textEmail;
+    public TextField textAddress;
+    public ListView<> friendList;
+    public Label lblName;
+    public Label lblPhone;
+    public Label lblEmail;
+    public Label lblAddress;
+
+    public void addFriend(ActionEvent actionEvent) {
+
+
+
+    }
+
+    public void displayFriend(MouseEvent mouseEvent) {
+
+
+
+    }
+
+    public void delFriend(ActionEvent actionEvent) {
+
+
+
+    }
+
+    public void editFriend(ActionEvent actionEvent) {
+
+
+
+    }
+
+}
