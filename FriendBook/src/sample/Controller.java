85d3f34ab2988470662429a0bba0fd0c3fbdb40e
832960 <55299454+832960@users.noreply.github.com>
2020-03-07 10:36:58 -0800

    Friend Book

--- FriendBook/src/sample/Controller.java
+++ FriendBook/src/sample/Controller.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -8,25 +9,49 @@ import javafx.scene.input.MouseEvent;
 
 public class Controller {
 
-    public TextField textName;
-    public TextField textPhone;
-    public TextField textEmail;
-    public TextField textAddress;
-    public ListView friendList;
+    public TextField textGetName;
+    public TextField textGetPhone;
+    public TextField textGetEmail;
+    public TextField textGetAddress;
+    public ListView<FriendBook> friendList = new ListView<>();
     public Label lblName;
     public Label lblPhone;
     public Label lblEmail;
     public Label lblAddress;
+    public Button btnEditFriend;
+    public Button btnDelFriend;
 
     public void addFriend(ActionEvent actionEvent) {
 
+        String name = textGetName.getText();
+        String phoneNumber = textGetPhone.getText();
+        String email = textGetEmail.getText();
+        String address = textGetAddress.getText();
 
+        FriendBook temp = new FriendBook(name, phoneNumber, email, address);
+        friendList.getItems().add(temp);
+
+        textGetName.clear();
+        textGetPhone.clear();
+        textGetEmail.clear();
+        textGetAddress.clear();
+
+        btnEditFriend.setDisable(true);
+        btnDelFriend.setDisable(true);
 
     }
 
     public void displayFriend(MouseEvent mouseEvent) {
 
+        FriendBook temp;
+        temp = friendList.getSelectionModel().getSelectedItem();
+        lblName.setText(temp.name);
+        lblPhone.setText(temp.getPhoneNumber());
+        lblEmail.setText(temp.getEmail());
+        lblAddress.setText(temp.getAddress());
 
+        btnEditFriend.setDisable(false);
+        btnDelFriend.setDisable(false);
 
     }
 
--- FriendBook/src/sample/FriendBook.java
+++ FriendBook/src/sample/FriendBook.java
@@ -1,4 +1,30 @@
 package sample;
 
 public class FriendBook {
+
+    public String name;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
+    FriendBook(String n, String p, String e, String a) {
+
+        name = n;
+        phoneNumber = p;
+        email = e;
+        address = a;
+
+    }
+
+    //Getters and Setters
+    public String getPhoneNumber() { return phoneNumber; }
+    public String getEmail() { return email; }
+    public String getAddress() { return address; }
+    public String toString() { return name; }
+
+    public void setName(String n) { name = n; }
+    public void setPhoneNumber(String p) { phoneNumber = p; }
+    public void setEmail(String e) { email = e; }
+    public void setAddress(String a) { address = a; }
+
 }
